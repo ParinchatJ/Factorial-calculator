ef9b287f7ed7a676463397ce716bae6af421b6ce
Parinchat Juntaworn <j.parinchat@gmail.com>
2023-02-24 18:23:22 +0700

    add fact logic in program by npm commander

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Factorial calculator
+const { program } = require("commander");
+
+program
+  .name("factorial-cal")
+  .description("A simple factorial calculator")
+  .version("1.0.0");
+
+program.command("fac <x>").action((x) => {
+  x = +x;
+
+  // check error
+  if (Number.isNaN(x)) {
+    throw new Error("Your input is not a number!");
+  }
+  let ans = +1;
+  for (let i = x; i >= 1; i--) {
+    ans *= i;
+  }
+  console.log(`the answer is ${ans}`);
+});
+
+program.parse();
